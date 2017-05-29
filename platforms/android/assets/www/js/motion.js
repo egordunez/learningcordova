6149efc85f341a57a5d3674ed384f61326c40a0e
Ernesto García Orduñez <egordunez@gmail.com>
2017-05-29 11:56:54 -0500

    Gradle updates

--- platforms/android/assets/www/js/motion.js
+++ platforms/android/assets/www/js/motion.js
@@ -16,3 +16,30 @@ function getAcceleration(){
    };
 	
 }
+
+
+function watchAcceleration(){
+    
+   var accelerometerOptions = {
+      frequency: 3000
+   }
+
+   var watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, accelerometerOptions);
+
+   function accelerometerSuccess(acceleration) {
+      alert('Acceleration X: ' + acceleration.x + '\n' +
+         'Acceleration Y: ' + acceleration.y + '\n' +
+         'Acceleration Z: ' + acceleration.z + '\n' +
+         'Timestamp: '      + acceleration.timestamp + '\n');
+
+      setTimeout(function() {
+         navigator.accelerometer.clearWatch(watchID);
+      }, 10000);
+
+   };
+
+   function accelerometerError() {
+      alert('onError!');
+   };
+	
+}
